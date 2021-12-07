c4036bd5ea3b806d4bf683fa419c6f91b3ee7b9f
ilsemeijer99 <73226038+ilsemeijer99@users.noreply.github.com>
2021-12-07 21:01:54 +0100

    NER

--- parsing.py
+++ parsing.py
@@ -1,13 +1,21 @@
 import spacy
+from spacy.tokens import Span
 
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("I love space, which exhibition should I go to?")
-# Analyze syntax
-print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
-print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+doc = nlp("One ticket for the Caixa museum, space, please.")
+interests = ['universe', 'space', 'cosmos', 'brain', 'interactive', 'big bang', 'evolution', 'science', 'life', 'physics', 'earth', 'sun', 'moon', 'mars', 'light', 'gravity', 'matter', 'planets', 'stars', 'nature', 'technology', 'floodings', 'forest', 'jungle', 'amazon', 'plants', 'water', 'ecosystem', 'biodiversity', 'brazil',
+             'south america', 'animals', 'climate', 'climate change', 'rivers', 'temperature', 'national geographic', 'photos', 'ecology', 'organisms', 'environment', 'photographer', 'polar', 'exploration', 'erosion', 'vulcanos', 'rock', 'rocks', 'landscape', 'earthquakes', 'green', 'sustainability', 'education', 'efficiency', 'building', 'architecture']
 
+exhibit_names = ['universe room', 'flooded forest',
+                 'antarctic base', 'geological wall', 'sustainable building']
+
+i = 0
 for token in doc:
-  print(str(token) + " : " + token.pos_ + " (" + token.lemma_ + ")")
+  if str(token) in interests:
+    doc.ents += (Span(doc, i, i+1, "INTEREST"),)
+  if str(token) in exhibit_names:
+    doc.ents += (Span(doc, i, i+1, "EXHIBITS"),)
+  i+=1
 
 # Find named entities, phrases and concepts
 for entity in doc.ents:

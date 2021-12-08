5533df93dbf2239df9e70ce4407835200cb26aff
Davide Locatelli <57136225+davidelct@users.noreply.github.com>
2021-12-08 17:11:59 +0100

    Add files via upload

---
+++ classify_intent.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from nltk import WordPunctTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import contractions
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pickle
+
+def clean_text(text):
+    text = contractions.fix(text)
+    text = text.lower()
+    stop_words = set(stopwords.words('english'))
+    text = text.split()
+    text = [w for w in text if not w in stop_words]
+    text = " ".join(text)
+    text = WordPunctTokenizer().tokenize(text)
+    lemm = WordNetLemmatizer()
+    text = [lemm.lemmatize(w) for w in text]
+    text = " ".join(text)
+    return text
+
+
+tix = pd.read_csv("data/augmented/tix.csv")
+interests = pd.read_csv("data/augmented/interests.csv")
+info = pd.read_csv("data/augmented/info.csv")
+df = pd.concat([tix, interests, info])
+df = df.sample(frac=1).reset_index(drop=True)
+df['phrase'] = df['phrase'].apply(clean_text)
+df.to_csv("df.csv",index=False)
+
+vectorizer = TfidfVectorizer()
+X = df['phrase'].tolist()
+X = vectorizer.fit_transform(X)
+pickle.dump(vectorizer, open("tfidf1.pkl", "wb"))
+y = df['class'].tolist()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
+model.fit(X_train, y_train)
+
+y_hat = model.predict(X_test)
+print(accuracy_score(y_test,y_hat))
+pickle.dump(model, open('model.sav', 'wb'))
---
+++ test_classifier.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from nltk import WordPunctTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import contractions
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+
+def clean_text(text):
+    text = contractions.fix(text)
+    text = text.lower()
+    stop_words = set(stopwords.words('english'))
+    text = text.split()
+    text = [w for w in text if not w in stop_words]
+    text = " ".join(text)
+    text = WordPunctTokenizer().tokenize(text)
+    lemm = WordNetLemmatizer()
+    text = [lemm.lemmatize(w) for w in text]
+    text = " ".join(text)
+    return text
+
+model = pickle.load(open('model.sav', 'rb'))
+tf1 = pickle.load(open("tfidf1.pkl", 'rb'))
+tf1_new = TfidfVectorizer(vocabulary = tf1.vocabulary_)
+classes = ["tix", "interests", "info"]
+query = "I've become really interested in mathematics"
+query = clean_text(query)
+query = tf1_new.fit_transform([query])
+pred = model.predict(query)
+print(classes[pred[0]])

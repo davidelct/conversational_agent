f22f74e89dae9125220e88aebfc3705066e21d32
Davide Locatelli <57136225+davidelct@users.noreply.github.com>
2021-12-08 16:18:12 +0100

    Add files via upload

--- augment_data.py
+++ augment_data.py
@@ -1,20 +1,51 @@
 from datetime import date, timedelta
 from dateutil.relativedelta import *
+from collections import OrderedDict
+from random import sample
 
 
-def augment(path_to_data, output_path):
+def augment(path_to_data, output_path, sample_size=0, short=False):
     numbers = ['two', 'three', 'four', 'five', 'six']
 
-    interests = ['universe', 'space', 'cosmos', 'brain', 'interactive', 'big bang', 'evolution', 'science', 'life', 'physics', 'earth', 'sun', 'moon', 'mars', 'light', 'gravity', 'matter', 'planets', 'stars', 'nature', 'technology', 'floodings', 'forest', 'jungle', 'amazon', 'plants', 'water', 'ecosystem', 'biodiversity', 'brazil',
-                 'south america', 'animals', 'climate', 'climate change', 'rivers', 'temperature', 'national geographic', 'photos', 'ecology', 'organisms', 'environment', 'photographer', 'polar', 'exploration', 'erosion', 'vulcanos', 'rock', 'rocks', 'landscape', 'earthquakes', 'green', 'sustainability', 'education', 'efficiency', 'building', 'architecture']
+    interests = ['universe', 'space', 'cosmos', 'brain', 'big bang', 'evolution', 'science', 'life', 'physics', 'earth', 'sun', 'moon', 'mars', 'light', 'gravity', 'matter', 'planets', 'stars', 'nature', 'technology', 'floodings', 'forest', 'jungle', 'amazon', 'plants', 'water', 'ecosystem', 'biodiversity', 'brazil',
+                 'south america', 'animals', 'climate', 'climate change', 'rivers', 'temperature', 'national geographic', 'photos', 'ecology', 'organisms', 'environment', 'photographer', 'exploration', 'erosion', 'vulcanos', 'rock', 'rocks', 'landscape', 'earthquakes', 'sustainability', 'education', 'efficiency', 'building', 'architecture']
 
     exhibit_names = ['universe room', 'flooded forest',
                      'antarctic base', 'geological wall', 'sustainable building']
 
     today = date.today()
-    end_date = today + relativedelta(months=+2)
+    end_date = today + relativedelta(months=+12)
     delta = end_date - today
     dates = [(today + timedelta(days=i)) for i in range(delta.days + 1)]
+    db = [day.strftime("%d %b") for day in dates]
+    bd = [day.strftime("%b %d") for day in dates]
+    db = ["for " + day for day in db]
+    bd = ["for " + day for day in bd]
+    dates1 = db
+    dates1 = dates1 + bd
+
+    days1 = ["for today", "for tomorrow"]
+    days1 = days1 + ["for Monday", "on Tuesday", "on Wednesday", "on Thursday", "on Friday", "on Saturday", "on Sunday"]
+    days1 = days1 + ["for this Monday", "for this Tuesday", "for this Wednesday", "for this Thursday", "for this Friday", "for this Saturday", "for this Sunday"]
+    days1 = days1 + ["for next Monday", "for next Tuesday", "for next Wednesday", "for next Thursday", "for next Friday", "for next Saturday", "for next Sunday"]
+    days1 = days1 + ["for this week", "for next week"]
+    days1 = days1 + ["for this weekend", "for next weekend"]
+    days1 = days1 + ["for this month", "for next month"]
+
+    db = [day.strftime("%d %b") for day in dates]
+    bd = [day.strftime("%b %d") for day in dates]
+    db = ["on " + day for day in db]
+    bd = ["on " + day for day in bd]
+    dates2 = db
+    dates2 = dates2 + bd
+
+    days2 = ["today", "tomorrow"]
+    days2 = days2 + ["on Monday", "on Tuesday", "on Wednesday", "on Thursday", "on Friday", "on Saturday", "on Sunday"]
+    days2 = days2 + ["this Monday", "this Tuesday", "this Wednesday", "this Thursday", "this Friday", "this Saturday", "this Sunday"]
+    days2 = days2 + ["next Monday", "next Tuesday", "next Wednesday", "next Thursday", "next Friday", "next Saturday", "next Sunday"]
+    days2 = days2 + ["this week", "next week"]
+    days2 = days2 + ["this weekend", "next weekend"]
+    days2 = days2 + ["this month", "next month"]
 
     f = open(path_to_data, 'r')
     phrases = f.readlines()
@@ -22,10 +53,10 @@ def augment(path_to_data, output_path):
     num_augmented_phrases = []
 
     for phrase in phrases:
-        tokenized = phrase.split()
-        if '[NUMBER]' in tokenized:
+        tokenized = phrase.lower().split()
+        if '[number]' in tokenized:
             for i, token in enumerate(tokenized):
-                if 'NUMBER' in token:
+                if 'number' in token:
                     break
             for num in numbers:
                 tokenized[i] = num
@@ -36,11 +67,15 @@ def augment(path_to_data, output_path):
     int_augmented_phrases = []
     for phrase in num_augmented_phrases:
         tokenized = phrase.split()
-        if '[EXHIBITION_TOPIC]' in tokenized:
+        if '[exhibition_topic]' in tokenized:
             for i, token in enumerate(tokenized):
-                if 'EXHIBITION_TOPIC' in token:
+                if 'exhibition_topic' in token:
                     break
-            for interest in interests:
+            if short:
+                rinterest = sample(interests, sample_size)
+            else:
+                rinterest = interests
+            for interest in rinterest:
                 tokenized[i] = interest
                 int_augmented_phrases.append(' '.join(tokenized))
         else:
@@ -49,9 +84,9 @@ def augment(path_to_data, output_path):
     exhibit_augmented_phrases = []
     for phrase in int_augmented_phrases:
         tokenized = phrase.split()
-        if '[EXHIBITION_NAME]' in tokenized:
+        if '[exhibition_name]' in tokenized:
             for i, token in enumerate(tokenized):
-                if 'EXHIBITION_NAME' in token:
+                if 'exhibition_name' in token:
                     break
             for exhibit in exhibit_names:
                 tokenized[i] = exhibit
@@ -62,17 +97,42 @@ def augment(path_to_data, output_path):
     dates_augmented_phrases = []
     for phrase in exhibit_augmented_phrases:
         tokenized = phrase.split()
-        if '[DATE]' in tokenized or '[DATE],' in tokenized:
+        if '[date1]' in tokenized:
             for i, token in enumerate(tokenized):
-                if 'DATE' in token:
+                if 'date1' in token:
                     break
-            for day in dates:
-                tokenized[i] = day.strftime("%d %b %Y")
+            if short:
+                rdates = sample(dates1, sample_size)
+                rdays = sample(days1, sample_size)
+            else:
+                rdates = dates1
+                rdays = days1
+            for d in rdates:
+                tokenized[i] = d
+                dates_augmented_phrases.append(' '.join(tokenized))
+            for d in rdays:
+                tokenized[i] = d
+                dates_augmented_phrases.append(' '.join(tokenized))
+        elif '[date2]' in tokenized:
+            for i, token in enumerate(tokenized):
+                if 'date2' in token:
+                    break
+            if short:
+                rdates = sample(dates2, sample_size)
+                rdays = sample(days2, sample_size)
+            else:
+                rdates = dates2
+                rdays = days2
+            for d in rdates:
+                tokenized[i] = d
+                dates_augmented_phrases.append(' '.join(tokenized))
+            for d in rdays:
+                tokenized[i] = d
                 dates_augmented_phrases.append(' '.join(tokenized))
         else:
             dates_augmented_phrases.append(' '.join(tokenized))
 
-    dates_augmented_phrases = list(set(dates_augmented_phrases))
+    dates_augmented_phrases = list(OrderedDict.fromkeys(dates_augmented_phrases))
 
     f = open(output_path, 'w')
     for phrase in dates_augmented_phrases:
@@ -80,8 +140,10 @@ def augment(path_to_data, output_path):
         f.write('\n')
 
 
-path_to_tix = 'data/tickets_phrases.txt'
-path_to_info = 'data/info_phrases.txt'
+path_to_tix = 'data/tix.txt'
+path_to_info = 'data/info.txt'
+path_to_interests = 'data/interests.txt'
 
-augment(path_to_tix, 'data/augmented/tix.txt')
-augment(path_to_info, 'data/augmented/info.txt')
+augment(path_to_tix, 'data/augmented/tix.txt', sample_size = 4, short=True)
+augment(path_to_info, 'data/augmented/info.txt', sample_size= 29, short=True)
+augment(path_to_interests, 'data/augmented/interests.txt')

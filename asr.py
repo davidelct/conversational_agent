b575b907d917b0a2394613f676ae317dd4be5f35
StephanieRO <86803264+StephanieRO@users.noreply.github.com>
2021-12-17 17:04:54 +0100

    Add files via upload
    last changes, working!!!

--- asr.py
+++ asr.py
@@ -1,7 +1,18 @@
 import speech_recognition as sr
-#obtain audio from the microphone
+
+#open microphone and obtaining audio
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    print("Say something!")
+    print("Hi there, How can I help you?")
     audio = r.listen(source)
-    print(r.recognize_google(audio))
+
+#recognize what you said and convert it to text
+try:
+    text = r.recognize_google(audio)
+    print("did you say that: {}".format(text))
+    #print(r.recognize_google(audio))
+    #errors responses 
+except sr.UnknownValueError:
+    print("I could not understand what you said")
+except sr.RequestError as e:
+    print("Apologies, I couldn't understand what you said; {0}".format(e))

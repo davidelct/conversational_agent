5b38f7cb8fb587d827179180871552e270f2a4cf
Davide Locatelli <57136225+davidelct@users.noreply.github.com>
2021-12-17 12:30:03 +0100

    Update asr.py

--- asr.py
+++ asr.py
@@ -1,16 +1,7 @@
 import speech_recognition as sr
-
-#obtaining audio from the microphone
+#obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    print("Say Something:")
-    r.adjust_for_ambient_noise(source)
+    print("Say something!")
     audio = r.listen(source)
-
-try:
-    text = r.recognize_google(audio)
-    print("You said : {}".format(text))
-except sr.UnknownValueError: 
-    print ("Could not undersatnd the audio")
-except sr.RequestError as e:
-    print("Sorry could not recognize what you said; {0}".format(e))
+    print(r.recognize_google(audio))

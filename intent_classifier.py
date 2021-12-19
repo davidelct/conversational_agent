3540d251dc8d415d996ae6fc8269b1fc64687005
Davide Locatelli <davide.lct@icloud.com>
2021-12-19 22:42:51 +0100

    fixed model directory

--- intent_classifier.py
+++ intent_classifier.py
@@ -3,8 +3,8 @@ import pickle
 from utils import clean_text
 
 def get_intent(query):
-    model = pickle.load(open('model.sav', 'rb'))
-    tf1 = pickle.load(open("tfidf1.pkl", 'rb'))
+    model = pickle.load(open('models/model.sav', 'rb'))
+    tf1 = pickle.load(open("models/tfidf1.pkl", 'rb'))
     tf1_new = TfidfVectorizer(vocabulary = tf1.vocabulary_)
     classes = ["tix", "interests", "info", "fallback", "welcome"]
     query = clean_text(query)

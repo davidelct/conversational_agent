d90cc104967afbf8e519ba4dce92f565004579c4
Davide Locatelli <57136225+davidelct@users.noreply.github.com>
2021-12-19 17:36:27 +0100

    new name for intent_classifier.py

---
+++ intent_classifier.py
@@ -0,0 +1,14 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+from utils import clean_text
+
+def get_intent(query):
+    model = pickle.load(open('model.sav', 'rb'))
+    tf1 = pickle.load(open("tfidf1.pkl", 'rb'))
+    tf1_new = TfidfVectorizer(vocabulary = tf1.vocabulary_)
+    classes = ["tix", "interests", "info", "fallback", "welcome"]
+    query = clean_text(query)
+    query = tf1_new.fit_transform([query])
+    pred = model.predict(query)
+    print(classes[pred[0]])
+    return classes[pred[0]]
